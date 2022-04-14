0c86ecd8e8da2968a301b8986fe8368bb1102c6b
ralphkw1 <ralphkw1@gmail.com>
2022-04-14 11:38:31 -0500

    Added more tests.

--- src/test/java/net/ddns/rkdawenterprises/brief4ijidea/plugin/Main_test.kt
+++ src/test/java/net/ddns/rkdawenterprises/brief4ijidea/plugin/Main_test.kt
@@ -7,7 +7,6 @@ package net.ddns.rkdawenterprises.brief4ijidea.plugin
 import com.intellij.remoterobot.RemoteRobot
 import com.intellij.remoterobot.fixtures.CommonContainerFixture
 import com.intellij.remoterobot.fixtures.ComponentFixture
-import com.intellij.remoterobot.fixtures.TextEditorFixture
 import com.intellij.remoterobot.search.locators.byXpath
 import com.intellij.remoterobot.stepsProcessing.step
 import com.intellij.remoterobot.utils.keyboard
@@ -20,12 +19,19 @@ import net.ddns.rkdawenterprises.brief4ijidea.plugin.pages.idea
 import net.ddns.rkdawenterprises.brief4ijidea.plugin.pages.welcomeFrame
 import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous
 import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.escape
+import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.get_current_line_number
+import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.get_end_offset
 import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.get_line
+import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.get_line_number
+import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.get_visible_area_bottom_offset_line
+import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.get_visible_area_top_offset_line
+import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.move_to_line
 import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Miscellaneous.Companion.scroll_to_line
 import net.ddns.rkdawenterprises.brief4ijidea.plugin.utils.Remote_robot_client
 import org.assertj.swing.core.MouseButton
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.awt.event.KeyEvent
 import java.awt.event.KeyEvent.VK_A
 import java.awt.event.KeyEvent.VK_ALT
 import java.awt.event.KeyEvent.VK_CONTROL
@@ -33,6 +39,7 @@ import java.awt.event.KeyEvent.VK_DELETE
 import java.awt.event.KeyEvent.VK_END
 import java.awt.event.KeyEvent.VK_ESCAPE
 import java.awt.event.KeyEvent.VK_H
+import java.awt.event.KeyEvent.VK_O
 import java.awt.event.KeyEvent.VK_SPACE
 import java.awt.event.KeyEvent.VK_U
 import java.time.Duration
@@ -106,11 +113,7 @@ class Main_test
             }
 
             step("Test the commands") {
-//                test_help_menu_command(textEditor())
-//                test_quick_java_doc_command(this)
-//                test_undo_redo_commands(textEditor())
-                test_change_output_file_command(textEditor())
-
+                test_commands()
             }
 
             println("Finished tests...")
@@ -118,8 +121,117 @@ class Main_test
         }
     }
 
-    private fun IdeaFrame.test_help_menu_command(text_editor_fixture: TextEditorFixture)
+    private fun IdeaFrame.test_commands()
     {
+//        test_help_menu_command()
+//        test_quick_java_doc_command()
+//        test_undo_redo_commands()
+//        test_change_output_file_command()
+//        test_beginning_of_line_command()
+//        test_end_of_line_command()
+        test_top_of_buffer_command()
+    }
+
+    private fun IdeaFrame.test_top_of_buffer_command()
+    {
+        val text_editor_fixture = textEditor()
+
+        step("Command: Top of buffer. Description: Moves the cursor to the first character of the buffer.")
+        {
+
+        }
+    }
+
+    private fun IdeaFrame.test_end_of_line_command()
+    {
+        val text_editor_fixture = textEditor()
+
+        step("Command: End of line. Description: Places the cursor at the last valid character of the current line, window, or file.")
+        {
+            val line = 159
+            val column = 68
+            text_editor_fixture.editor.move_to_line(line,
+                                                    Miscellaneous.Companion.Column_target.END)
+            val end_of_line_offset = text_editor_fixture.editor.caretOffset
+            text_editor_fixture.editor.move_to_line(line,
+                                                    column)
+            val window_end_offset_line = text_editor_fixture.editor.get_visible_area_bottom_offset_line()
+                .split(",")
+                .toTypedArray()
+            val window_end_offset = window_end_offset_line[0].toInt()
+            val window_end_line = window_end_offset_line[1].toInt()
+            val window_end_offset_alternate = window_end_offset_line[2].toInt()
+            val window_end_line_alternate = window_end_offset_line[3].toInt()
+
+            val starting_offset = text_editor_fixture.editor.caretOffset
+            assert(starting_offset != end_of_line_offset)
+
+            keyboard { key(VK_END) }
+            var current_offset = text_editor_fixture.editor.caretOffset
+            var current_line = text_editor_fixture.editor.get_current_line_number()
+            assert((current_offset == end_of_line_offset) && (current_line == line))
+
+            keyboard { key(VK_END) }
+            current_offset = text_editor_fixture.editor.caretOffset
+            current_line = text_editor_fixture.editor.get_current_line_number()
+            assert(((current_offset == window_end_offset) && (current_line == window_end_line)) ||
+                           (current_offset == window_end_offset_alternate) && ((current_line == window_end_line_alternate)))
+
+            keyboard { key(VK_END) }
+            current_offset = text_editor_fixture.editor.caretOffset
+            current_line = text_editor_fixture.editor.get_current_line_number()
+            val end_offset = text_editor_fixture.editor.get_end_offset()
+            val end_line = text_editor_fixture.editor.get_line_number(end_offset)
+            assert((current_line == end_line) && (current_offset == end_offset))
+        }
+    }
+
+    private fun IdeaFrame.test_beginning_of_line_command()
+    {
+        val text_editor_fixture = textEditor()
+
+        step("Command: Beginning of line. Description: Places the cursor at column 1 of the current line, window, or file.")
+        {
+            val line = 159
+            val column = 68
+            text_editor_fixture.editor.move_to_line(line)
+            val beginning_of_line_offset = text_editor_fixture.editor.caretOffset
+            text_editor_fixture.editor.move_to_line(line,
+                                                    column)
+
+            val window_beginning_offset_line = text_editor_fixture.editor.get_visible_area_top_offset_line()
+                .split(",")
+                .toTypedArray()
+            val window_beginning_offset = window_beginning_offset_line[0].toInt()
+            val window_beginning_line = window_beginning_offset_line[1].toInt()
+            val window_beginning_offset_alternate = window_beginning_offset_line[2].toInt()
+            val window_beginning_line_alternate = window_beginning_offset_line[3].toInt()
+
+            val starting_offset = text_editor_fixture.editor.caretOffset
+            assert(starting_offset != beginning_of_line_offset)
+
+            keyboard { key(KeyEvent.VK_HOME) }
+            var current_offset = text_editor_fixture.editor.caretOffset
+            var current_line = text_editor_fixture.editor.get_current_line_number()
+            assert((current_offset == beginning_of_line_offset) && (current_line == line))
+
+            keyboard { key(KeyEvent.VK_HOME) }
+            current_offset = text_editor_fixture.editor.caretOffset
+            current_line = text_editor_fixture.editor.get_current_line_number()
+            assert(((current_line == window_beginning_line) && (current_offset == window_beginning_offset)) ||
+                           ((current_line == window_beginning_line_alternate) && (current_offset == window_beginning_offset_alternate)))
+
+            keyboard { key(KeyEvent.VK_HOME) }
+            current_offset = text_editor_fixture.editor.caretOffset
+            current_line = text_editor_fixture.editor.get_current_line_number()
+            assert((current_line == 0) && (current_offset == 0))
+        }
+    }
+
+    private fun IdeaFrame.test_help_menu_command()
+    {
+        val text_editor_fixture = textEditor()
+
         step("Command: Help. Description: Open general help menu.")
         {
             text_editor_fixture.editor.keyboard {
@@ -129,19 +241,20 @@ class Main_test
 
             Thread.sleep(5000)
 
-            waitFor(Duration.ofSeconds(30))
+            waitFor(Duration.ofSeconds(60))
             {
                 text_editor_fixture.editor.hasFocus
             }
         }
     }
 
-    private fun IdeaFrame.test_quick_java_doc_command(idea_frame: IdeaFrame)
+    private fun IdeaFrame.test_quick_java_doc_command()
     {
+        val text_editor_fixture = textEditor()
+
         step("Command: Quick Java Doc. Description: Show contextual documentation popup.")
         {
-            val text_editor_fixture: TextEditorFixture = idea_frame.textEditor()
-            text_editor_fixture.editor.scroll_to_line(41);
+            text_editor_fixture.editor.scroll_to_line(42);
             waitFor { text_editor_fixture.editor.hasText("SuppressWarnings") }
             text_editor_fixture.editor.findText("SuppressWarnings")
                 .click()
@@ -150,10 +263,10 @@ class Main_test
                        VK_H)
             }
 
-            waitFor { idea_frame.heavyWeightWindows().size == 1 }
-            val all_text = idea_frame.heavyWeightWindows()[0].find(CommonContainerFixture::class.java,
-                                                                   byXpath("//div[@class='JEditorPane']"),
-                                                                   Duration.ofSeconds(5))
+            waitFor { heavyWeightWindows().size == 1 }
+            val all_text = heavyWeightWindows()[0].find(CommonContainerFixture::class.java,
+                                                        byXpath("//div[@class='JEditorPane']"),
+                                                        Duration.ofSeconds(5))
                 .findAllText()
             var found_it = false
             for(i in all_text.indices)
@@ -168,15 +281,17 @@ class Main_test
         }
     }
 
-    private fun IdeaFrame.test_undo_redo_commands(text_editor_fixture: TextEditorFixture)
+    private fun IdeaFrame.test_undo_redo_commands()
     {
-        var string_at_48_modified: String = ""
+        val text_editor_fixture = textEditor()
+        val line = 49
+        var string_at_line_modified: String = ""
 
         step("Command: Undo. Description: Reverses the effect of any typing or commands that modified an open file.")
         {
             keyboard { key(VK_ESCAPE) }
 
-            val string_at_48 = text_editor_fixture.editor.get_line(48)
+            val string_at_line = text_editor_fixture.editor.get_line(line)
 
             val index = 14
             val offset = text_editor_fixture.editor.caretOffset
@@ -184,9 +299,9 @@ class Main_test
             waitFor { text_editor_fixture.editor.caretOffset == (offset + index) }
             keyboard { key(VK_DELETE) }
             keyboard { key(VK_DELETE) }
-            string_at_48_modified = text_editor_fixture.editor.get_line(48)
-            assert(string_at_48_modified == string_at_48.removeRange(index,
-                                                                     index + 2))
+            string_at_line_modified = text_editor_fixture.editor.get_line(line)
+            assert(string_at_line_modified == string_at_line.removeRange(index,
+                                                                         index + 2))
 
             text_editor_fixture.editor.keyboard {
                 hotKey(VK_ALT,
@@ -198,8 +313,8 @@ class Main_test
                        VK_U)
             }
 
-            val string_at_48_modified_undo = text_editor_fixture.editor.get_line(48)
-            assert(string_at_48 == string_at_48_modified_undo)
+            val string_at_line_modified_undo = text_editor_fixture.editor.get_line(line)
+            assert(string_at_line == string_at_line_modified_undo)
         }
 
         step("Command: Redo. Description: Reverses the effect of commands that have been undone.")
@@ -214,8 +329,8 @@ class Main_test
                        VK_U)
             }
 
-            val string_at_48_modified_redo = text_editor_fixture.editor.get_line(48)
-            assert(string_at_48_modified_redo == string_at_48_modified)
+            val string_at_line_modified_redo = text_editor_fixture.editor.get_line(line)
+            assert(string_at_line_modified_redo == string_at_line_modified)
 
             text_editor_fixture.editor.keyboard {
                 hotKey(VK_ALT,
@@ -229,13 +344,20 @@ class Main_test
         }
     }
 
-    private fun IdeaFrame.test_change_output_file_command(text_editor_fixture: TextEditorFixture)
+    private fun IdeaFrame.test_change_output_file_command()
     {
+        val text_editor_fixture = textEditor()
+
         step("Command: Change output file. Description: Changes the output file name for the current buffer.")
         {
             text_editor_fixture.editor.keyboard {
                 hotKey(VK_ALT,
-                       VK_H)
+                       VK_O)
+            }
+
+            dialog("Rename",
+                   Duration.ofSeconds(60)) {
+                button("Cancel").click()
             }
         }
     }
--- src/test/java/net/ddns/rkdawenterprises/brief4ijidea/plugin/utils/Miscellaneous.kt
+++ src/test/java/net/ddns/rkdawenterprises/brief4ijidea/plugin/utils/Miscellaneous.kt
@@ -1,4 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+@file:Suppress("ClassName")
+
 package net.ddns.rkdawenterprises.brief4ijidea.plugin.utils
 
 import com.intellij.remoterobot.RemoteRobot
@@ -66,22 +68,39 @@ class Miscellaneous(private val remoteRobot: RemoteRobot)
                 """
                     const editor = local.get('editor')
                     const document = local.get('document')
-                    const offset = document.getLineStartOffset(${line_number} - 1)
+                    const offset = document.getLineStartOffset($line_number)
                     editor.getScrollingModel().scrollTo(editor.offsetToLogicalPosition(offset), com.intellij.openapi.editor.ScrollType.CENTER)
                 """.trimIndent();
             runJs(script, true)
             Thread.sleep(500)
         }
 
+        enum class Column_target { START, END }
+
         @JvmStatic
-        fun EditorFixture.move_to_line(line_number: Int)
+        fun EditorFixture.move_to_line(line_number: Int, column_target: Column_target = Column_target.START)
+        {
+            if(column_target == Column_target.START)
+            {
+                move_to_line(line_number,
+                             0)
+            }
+            else if(column_target == Column_target.END)
+            {
+                move_to_line(line_number,
+                             get_end_column(line_number))
+            }
+        }
+
+        @JvmStatic
+        fun EditorFixture.move_to_line(line_number: Int, column_number: Int)
         {
             val script =
                 """
                     importClass(com.intellij.openapi.editor.ScrollType)
                     const editor = component.getEditor()
                     const document = editor.getDocument()
-                    const offset = document.getLineStartOffset($line_number - 1)
+                    const offset = document.getLineStartOffset($line_number) + $column_number
                     editor.getScrollingModel().scrollTo(editor.offsetToLogicalPosition(offset), ScrollType.CENTER)
                     const visual_position = editor.offsetToVisualPosition(offset)
                     editor.visualPositionToXY(visual_position)
@@ -114,6 +133,159 @@ class Miscellaneous(private val remoteRobot: RemoteRobot)
             Thread.sleep(500)
 
             return selectedText
+        }
+
+        /**
+         * Obtains the caret offset and line number of the beginning of the first line in the current visible area.
+         * Also returns an alternate (the next line) offset/line since scrolling may not allow the caret
+         * to be on the first visible line.
+         *
+         * @return CSV string with "offset,line,offset,line".
+         */
+        @JvmStatic
+        fun EditorFixture.get_visible_area_top_offset_line(): String
+        {
+            val script =
+                """
+                    importPackage(com.intellij.util)
+                    
+                    const editor = local.get('editor')
+                    const document = local.get('document')
+                    
+                    const is_true_smooth_scrolling_enabled = SystemProperties.isTrueSmoothScrollingEnabled()
+                    const model = editor.scrollingModel
+                    const visible_area = is_true_smooth_scrolling_enabled ? model.getVisibleAreaOnScrollingFinished() : model.getVisibleArea()
+                    const visible_area_top_line_number = editor.yToVisualLine( visible_area.y )
+                    if( ( visible_area.y > editor.visualLineToY( visible_area_top_line_number ) ) &&
+                            ( ( visible_area.y + visible_area.height ) > editor.visualLineToY( visible_area_top_line_number + 1 ) ) )
+                    {
+                        visible_area_top_line_number++;
+                    }
+                    
+                    const offset = document.getLineStartOffset(visible_area_top_line_number)
+                    const alternate_offset = document.getLineStartOffset(visible_area_top_line_number + 1)
+                    
+                    offset.toString() + ',' + visible_area_top_line_number.toString() + ',' +
+                    alternate_offset.toString() + ',' + (visible_area_top_line_number + 1).toString()
+                """.trimIndent();
+            return callJs(script, true)
+        }
+
+        /**
+         * Obtains the caret offset and line number of the end of the last line in the current visible area.
+         * Also returns an alternate (the previous line) offset/line since scrolling may not allow the caret
+         * to be on the last visible line.
+         *
+         * @return CSV string with "offset,line,offset,line".
+         */
+        @JvmStatic
+        fun EditorFixture.get_visible_area_bottom_offset_line(): String
+        {
+            val script =
+                """
+                    importPackage(com.intellij.util)
+                    importPackage(com.intellij.openapi.editor.ex.util)
+                    
+                    const editor = local.get('editor')
+                    const document = local.get('document')
+                    
+                    const is_true_smooth_scrolling_enabled = SystemProperties.isTrueSmoothScrollingEnabled()
+                    const model = editor.scrollingModel
+                    const visible_area = is_true_smooth_scrolling_enabled ? model.getVisibleAreaOnScrollingFinished() : model.getVisibleArea()
+                    const max_Y = visible_area.y + visible_area.height - editor.getLineHeight();
+                    const visible_area_bottom_line_number = editor.yToVisualLine( max_Y );
+                    if( ( visible_area_bottom_line_number > 0 ) &&
+                        ( max_Y < editor.visualLineToY( visible_area_bottom_line_number ) ) &&
+                        ( visible_area.y <= editor.visualLineToY( visible_area_bottom_line_number - 1 ) ) )
+                    {
+                        visible_area_bottom_line_number--;
+                    }
+                    
+                    const offset = document.getLineStartOffset(visible_area_bottom_line_number) +
+                        EditorUtil.getLastVisualLineColumnNumber( editor,
+                                                                  visible_area_bottom_line_number )
+                    const alternate_offset = document.getLineStartOffset(visible_area_bottom_line_number) +
+                        EditorUtil.getLastVisualLineColumnNumber( editor,
+                                                                  visible_area_bottom_line_number - 1 )
+                    
+                    offset.toString() + ',' + visible_area_bottom_line_number.toString() + ',' +
+                    alternate_offset.toString() + ',' + (visible_area_bottom_line_number - 1).toString()
+                """.trimIndent();
+            return callJs(script, true)
+        }
+
+        @JvmStatic
+        fun EditorFixture.get_current_line_number(): Int
+        {
+            val script =
+                """
+                    const editor = local.get('editor')
+                    const document = local.get('document')
+                    editor.offsetToLogicalPosition(editor.getCaretModel().getOffset()).line
+                """.trimIndent();
+            return callJs(script, true)
+        }
+
+        @JvmStatic
+        fun EditorFixture.get_line_number(offset: Int): Int
+        {
+            val script =
+                """
+                    const editor = local.get('editor')
+                    editor.offsetToLogicalPosition($offset).line
+                """.trimIndent();
+            return callJs(script, true)
+        }
+
+        @JvmStatic
+        fun EditorFixture.get_start_offset(line_number: Int): Int
+        {
+            val script =
+                """
+                    const editor = local.get('editor')
+                    const document = local.get('document')
+                    document.getLineStartOffset($line_number)
+                """.trimIndent();
+            return callJs(script, true)
+        }
+
+        @JvmStatic
+        fun EditorFixture.get_end_column(line_number: Int): Int
+        {
+            val script =
+                """
+                    importPackage(com.intellij.openapi.editor.ex.util)
+                                        
+                    const editor = local.get('editor')
+                    EditorUtil.getLastVisualLineColumnNumber( editor, $line_number )
+                """.trimIndent();
+            return callJs(script, true)
+        }
+
+        @JvmStatic
+        fun EditorFixture.get_end_offset(line_number: Int): Int
+        {
+            val script =
+                """
+                    importPackage(com.intellij.openapi.editor.ex.util)
+                                        
+                    const editor = local.get('editor')
+                    const document = local.get('document')
+                    document.getLineStartOffset($line_number) +
+                        EditorUtil.getLastVisualLineColumnNumber( editor, $line_number )
+                """.trimIndent();
+            return callJs(script, true)
+        }
+
+        @JvmStatic
+        fun EditorFixture.get_end_offset(): Int
+        {
+            val script =
+                """
+                    const document = local.get('document')
+                    document.getTextLength()
+                """.trimIndent();
+            return callJs(script, true)
         }
     }
 }
